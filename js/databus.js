8a00e6f7b7cd6d3fc126a2e7a695e76dd1efa95e
wanglekai <xiaoguailele@foxmail.com>
2021-11-19 23:20:41 +0800

    add: Main 类和 Databus 类

---
+++ js/databus.js
@@ -0,0 +1,10 @@
+let instance
+
+export default class Databus {
+    constructor () {
+        if (instance) return instance
+        instance = this
+        this.canvas
+        this.ctx
+    }
+}
--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
+import Databus from './databus.js'
+const databus = new Databus()
 
 export default class Main {
   constructor () {
-   
+   // 获取 canvas 画布，以及绘图上下文
+   this.canvas = wx.createCanvas()
+   this.ctx = this.canvas.getContext('2d')
+   databus.canvas = this.canvas
+   databus.ctx = this.ctx
   }
 }
